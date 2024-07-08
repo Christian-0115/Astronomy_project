2544670b599dc146f2c0886db467af14bff84047
Christian-0115 <107576872+Christian-0115@users.noreply.github.com>
2024-07-07 21:41:02 -0500

    Add files via upload

---
+++ Tester.py
@@ -0,0 +1,52 @@
+import astronomy_functions as af
+import MDM_targets as md
+import numpy as np
+from math import pi
+from astropy import units as u
+from astropy import constants as const
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from scipy import stats
+#Declare Variables and constants
+SUN_MASS = const.M_sun # Value = 1.98840987e+30 kg
+
+
+def KIC_Model(KICnumber):
+    #Declare paramters
+    target = str(KICnumber)
+    time_array = af.KIC_6425783_time_of_observation *u.day
+    mass1 = md.target_mass(KICnumber) * SUN_MASS
+    target_period = md.target_period(KICnumber)
+    
+    #Find minimum Chi-Square value and respective modeled parameters
+    minimum = af.minimum(KICnumber)
+    min_ = minimum
+    b_f_theta = np.array([min_[0].value]) * u.radian
+    b_f_q = np.array([min_[1]])
+    b_f_i = np.array([min_[2]])
+    min_chi_value = np.array([min_[3]])
+    #print(b_f_theta,b_f_q,b_f_i)
+    
+    #calculate best fit curve radial velocity and plot it with measured rv
+    b_f_rv = af.calculate_radial_velocity(mass1, b_f_q, time_array, target_period, b_f_theta, b_f_i)
+    measured_rv = af.KIC_6425783_measured_RVs 
+    sns.set()
+    ax = plt.axes()
+    ax.set(xlabel = 'time (days)', ylabel = 'radial velocity (km/sec)',
+           title = 'Motion of star 1 around star 2')
+    plt.plot(time_array, (b_f_rv[:,0,0,0].to(u.km/u.s)).value, label = 'best fit rv curve', linestyle = ':')
+    plt.plot(time_array,measured_rv, label = 'measured rv', linestyle = '-')
+    plt.legend()
+    plt.show()
+    
+    #Hypothesis testing
+    print('Experimental p value = ', min_chi_value)
+    right_tail_value = stats.chi2.ppf(1-0.025, df = 2)#we use 2 degrees of freedom since we have 3 categories of modeled parameters, dof = n-1 = (theta, q, inc_angle) - 1
+    print('Theoretical p value =', right_tail_value)
+    if min_chi_value < right_tail_value:
+        print('We have a binary system')
+    elif min_chi_value > right_tail_value:
+        print('We do not have a binary system')
+            
+            
